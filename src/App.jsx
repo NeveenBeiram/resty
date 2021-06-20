1b61330bfbde071cbc7ac64af1e588a133f869ad
NeveenBeiram <neveenbeiram@yahoo.com>
2021-06-20 22:51:58 +0300

    not done yet

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss';
+import React from 'react';
+import Header from './header';
+import Main from './main';
+import Footer from './footer';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Main />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
